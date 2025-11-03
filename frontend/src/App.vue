b9683dd20358259d17b9a4c1dd54685d2bec38f4
Tiago Antolagic Duarte <tiago.antolagic-duarte@telefonica.com>
2025-11-03 14:02:57 +0100

    added Hallroom or movie room component and tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,26 @@
-<script setup>
-import HallRoom from './components/HallRoom.vue';
-
+<script>
+import HallRoom from "./components/HallRoom.vue";
+export default {
+  components: {
+    HallRoom,
+  },
+  data() {
+    return {};
+  },
+  emits: ["movie"],
+  methods: {
+    goToMovie(movie) {
+      console.log(movie);
+    },
+  },
+};
 </script>
 
 <template>
-<HallRoom></HallRoom>
-
+  <div
+    class="min-h-screen min-w-screen bg-linear-to-br from-gray-900 via-gray-800 to-black text-white overflow-hidden"
+  >
+    <HallRoom @movie="goToMovie"></HallRoom>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/components/HallRoom.vue
+++ frontend/src/components/HallRoom.vue
@@ -1,16 +1,22 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      movies: [],
+    };
   },
   methods: {
     async getMovies() {
       const response = await fetch("http://localhost:8080/movie", {
         method: "GET",
-        "Content-Type": "application/json",
+        headers: { "Content-Type": "application/json" },
       });
       const data = await response.json();
       console.log(data);
+      this.movies = data;
+    },
+    showMovie(movie) {
+      this.$emit("movie", movie);
     },
   },
   async mounted() {
@@ -19,6 +25,20 @@ export default {
 };
 </script>
 <template>
-  <div></div>
+  <div>
+    <div class="flex justify-center mt-10">
+      <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <li v-for="(movie, index) in movies" :key="index">
+          <div
+            class="flex justify-center items-center flex-col p-4 shadow-lg rounded-xl gap-2 text-black hover:cursor-pointer duration-300 hover:scale-105 hover:shadow-2xl transform transition bg-white"
+            @click="showMovie(movie)"
+          >
+            <h2 class="text-lg font-bold">{{ movie.titel }}</h2>
+            <p class="text-gray-700">{{ movie.movieDate }}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <style scoped></style>
